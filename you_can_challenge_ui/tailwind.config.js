519d460b922dc554575f5174315d1d1496d2c690
zack965 <elhossnizakaria@gmail.com>
2023-11-11 20:19:46 +0100

    adding dependencies and tailwindcss

---
+++ you_can_challenge_ui/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    'node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+
+}
+

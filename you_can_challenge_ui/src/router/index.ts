e77b76a1009ad8d6b414cc35c413c32f21273e75
zack965 <elhossnizakaria@gmail.com>
2023-11-11 20:17:38 +0100

    updating routes in ui

--- you_can_challenge_ui/src/router/index.ts
+++ you_can_challenge_ui/src/router/index.ts
@@ -1,10 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
+import ProductsListVue from '../views/ProductsList.vue'
+import CreateProduct from '../views/CreateProduct.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: ProductsListVue
+    },
+    {
+      path: '/create-product',
+      name: 'create-product',
+      component: CreateProduct
+    }
+    /*  {
       path: '/',
       name: 'home',
       component: HomeView
@@ -16,7 +27,7 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    } */
   ]
 })
 
